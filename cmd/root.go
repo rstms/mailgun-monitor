511674f1336f94a9b7407b322a065895349cec37
Matt Krueger <mkrueger@rstms.net>
2025-11-19 08:38:25 -0700

    Bump version: 0.1.1 → 0.1.2

--- cmd/root.go
+++ cmd/root.go
@@ -45,7 +45,7 @@ var logFile *os.File
 var rootCmd = &cobra.Command{
 	Use:     "mailgun-monitor",
 	Short:   "mailgun toolkit",
-	Version: "0.1.1",
+	Version: "0.1.2",
 	Long: `
 Functions making use of the mailgun API
 `,
